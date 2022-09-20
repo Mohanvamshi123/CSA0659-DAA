7217daea896c4fe618aff7030973d2b327839646
Mohanvamshi123 <113008509+Mohanvamshi123@users.noreply.github.com>
2022-09-20 20:52:13 +0530

    Add files via upload

---
+++ palindromestring or not.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str1[50],srt2[50];
+print("\n enter the string to be check the palindrom:")
+gets(str1);
+strcpy(str2,str1);
+strrev(str2);
+if(strcpy(str1,str2)==0)
+printf("string is palindrome:")
+else
+printf("string is not palindrome:")
+return 0;
+}
