8fdf978d10ddcace2932a67ec6c1975d97dad6e6
Mohanvamshi123 <113008509+Mohanvamshi123@users.noreply.github.com>
2022-09-20 20:55:13 +0530

    Add files via upload

---
+++ min and max of elements.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int a[100],i,n,min,max;
+printf("enter the size of array:");
+scanf("%d",&n);
+printf("enter the elements:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+min=max=a[0];
+for(i=0;i<n;i++)
+{
+if(min>a[i])
+  min=a[i];
+if(max<a[i])
+  max=a[i];
+  }
+  printf("min of array is %d",min);
+
+  printf("max of arrar is %d",max);
+  return 0;
+  }
+
